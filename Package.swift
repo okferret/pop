21f1cedf71146ad6f2f528b8e5a4c2ba18630457
okferret <wanggang3@staff.sina.com.cn>
2025-03-24 11:31:42 +0800

    update => swift package init

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+// carthage build --no-skip-current --use-xcframeworks
+
+import PackageDescription
+
+let package = Package(
+    name: "pop",
+    platforms: [.iOS(.v13), .macOS(.v10_15)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "pop",
+            targets: ["pop"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(
+            name: "pop",
+            path: "./Carthage/Build/pop.xcframework"),
+        .testTarget(
+            name: "popTests",
+            dependencies: ["pop"]
+        ),
+    ]
+)
